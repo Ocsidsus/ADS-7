0d05fd6615471e7194b87633175c5ac5a08a41ed
Dmitry Volkov <83473625+Ocsidsus@users.noreply.github.com>
2022-05-01 19:54:20 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,6 +1,7 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include <string>
 
 template<typename T>
 class TPQueue {
@@ -51,7 +52,9 @@ class TPQueue {
             return data;
         }
         else {
+            T error;
             throw std::string(" Empty ! ");
+            return error;
         }
     }
  private:
